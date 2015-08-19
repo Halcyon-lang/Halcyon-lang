67649b260b2a187adce396d1e9e350dfe85d41a1
magnusi <luk.hozda@gmail.com>
2015-08-19 23:24:36 +0200

    Added Utils.cs

---
+++ Halcyon C extensions/Utils.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define Dll   __declspec( dllexport )
+
+Dll void printHelp() 
+{
+	printf("SlashButter compiler help:\n");
+	printf("   -compile [File] - Compiles file to executable\n");
+	printf("   -convert [File] - Converts file to IL. File's will be written down here\n");
+	printf("   -result [Text] - how does given line look in IL\n");
+	return;
+}
+
+Dll void printInfoHelp() 
+{
+	printf("-info\n");
+	printf("   classes - Prints all classes\n");
+	printf("   elements - Prints currently loaded elements\n");
+	printf("   version - Prints current version of SlashButter\n");
+	return;
+}
+
+int main() {}
+
+
---
+++ Halcyon/Utils.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Halcyon
+{
+    class Utils
+    {
+        [DllImport("Utils.dll")]
+        public static extern void printHelp();
+        [DllImport("Utils.dll")]
+        public static extern void printInfoHelp();
+
+        public static void giveInfo(string arg)
+        {
+            switch (arg)
+            {
+                case "elements":
+                    Console.WriteLine();
+                    foreach (Type type in Assembly.GetCallingAssembly().GetExportedTypes())
+                    {
+                        if (type.IsSubclassOf(typeof(Element)))
+                        {
+                            Console.WriteLine(type.Name);
+                        }
+                    }
+                    Console.WriteLine();
+                    break;
+                case "classes":
+                    Console.WriteLine();
+                    foreach (Type type in Assembly.GetCallingAssembly().GetExportedTypes())
+                    {
+                        Console.WriteLine(type.Name);
+                    }
+                    Console.WriteLine();
+                    break;
+                case "version":
+                    Console.WriteLine();
+                    Console.WriteLine(Assembly.GetExecutingAssembly().GetName().Version);
+                    Console.WriteLine();
+                    break;
+                case "help":
+                    Utils.printInfoHelp();
+                    break;
+                default:
+                    Errors.Exceptions.Exception(0);
+                    break;
+            }
+        }
+    }
+}
