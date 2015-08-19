d880f8838274ec31ef38b0162c7a2670ffbd198e
magnusi <luk.hozda@gmail.com>
2015-08-19 23:25:01 +0200

    Added Compiler. Needs major revision

---
+++ Halcyon/Compiler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Halcyon
+{
+    public static class Compiler
+    {
+        public static void checkArgs(string[] args)
+        {
+            switch (args[0])
+            {
+                case "-compile":
+                    Console.WriteLine("And here would I place normal compiler, IF I HAD ONE");
+                    Console.Write("SlashButter:");
+                    Console.Title = "SlashButter Compiler";
+                    HalcyonConsole.Command(Console.ReadLine());
+                    break;
+                case "-help":
+                    Utils.printHelp();
+                    Console.Write("SlashButter:");
+                    HalcyonConsole.Command(Console.ReadLine());
+                    break;
+                case "-convert":
+                    Console.WriteLine("And here would I place conversion-only compiler, IF I HAD ONE");
+                    Console.Write("SlashButter:");
+                    Console.Title = "SlashButter Compiler";
+                    HalcyonConsole.Command(Console.ReadLine());
+                    break;
+                case "-result":
+                    Console.WriteLine("And here would I place line result, IF I HAD ONE");
+                    Console.Write("SlashButter:");
+                    Console.Title = "SlashButter Compiler";
+                    HalcyonConsole.Command(Console.ReadLine());
+                    break;
+                case "-info":
+                    Utils.giveInfo(args[1]);
+                    Console.Write("SlashButter:");
+                    Console.Title = "SlashButter Compiler";
+                    HalcyonConsole.Command(Console.ReadLine());
+                    break;
+                default:
+                    Errors.Exceptions.Exception(3);
+                    Console.Write("SlashButter:");
+                    Console.Title = "SlashButter Compiler";
+                    HalcyonConsole.Command(Console.ReadLine());
+                    break;
+            }
+
+        }
+    }
+}
