200c3c31b39efec48fcfd33fbe6189d069f24e1f
magnusi <luk.hozda@gmail.com>
2015-08-19 23:25:38 +0200

    ... and everything else

---
+++ Halcyon/HalcyonConsole.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Halcyon
+{
+    class HalcyonConsole
+    {
+        public static void Command(string str) 
+        {
+            string[] args = str.Split(Convert.ToChar(" "));
+            switch (args.Count())
+            {
+                case 0:
+                    //Utils.printHelp();
+                    Console.Write("SlashButter:");
+                    Console.Title = "SlashButter Compiler";
+                    HalcyonConsole.Command(Console.ReadLine());
+                    break;
+                case 1:
+                    if (args[0] != "-compile" && args[0] != "-result" && args[0] != "-convert" && args[0] != "-info" && args[0] != "-help" && !string.IsNullOrEmpty(args[0])) Errors.Exceptions.Exception(4);
+                    else if (string.IsNullOrEmpty(args[0]))
+                    {
+                        Console.Write("SlashButter:");
+                        HalcyonConsole.Command(Console.ReadLine());
+                    }
+                    else if (args[0] == "-info") Utils.printInfoHelp();
+                    else if (args[0] == "-help") Utils.printHelp();
+                    else
+                    {
+                        Errors.Exceptions.Exception(0);
+                        Console.WriteLine();
+                        Utils.printHelp();
+                    }
+                    Console.Write("SlashButter:");
+                    Console.Title = "SlashButter Compiler";
+                    HalcyonConsole.Command(Console.ReadLine());
+                    break;
+                case 2:
+                    Compiler.checkArgs(args);
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Halcyon/Preprocessor.cs
@@ -0,0 +1,132 @@
+﻿using Halcyon.Errors;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Halcyon
+{
+    public delegate void DirectiveCallback(string line);
+    class Preprocessor
+    {
+        public static string[] InputFile;
+        public static string PreprocessedFile = "";
+        public static string FilePath;
+        public static List<Directive> DirectiveList = new List<Directive>();
+        public static void LoadFile(string path) 
+        {
+            string realPath;
+            if (path.StartsWith("-\\")) 
+            {
+                realPath = Path.Combine(Environment.CurrentDirectory, path.Substring(2));
+            }
+            else
+            {
+                realPath = path;
+            }
+            FilePath = realPath;
+            Console.WriteLine("Path set to " + realPath);
+            if (System.IO.File.Exists(realPath))
+            {
+                ReadFile(realPath);
+            }
+        }
+        public static void ReadFile(string path)
+        {
+            if (!path.Contains(".halcyon"))
+            {
+                Exceptions.Exception(5);
+            }
+            else
+            {
+                try
+                {
+                    InputFile = System.IO.File.ReadAllLines(path);
+                    Preproccess();
+                }
+                catch
+                {
+                    Exceptions.Exception(6);
+                }
+            }
+        }
+
+        public static void Preproccess()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void initDirectives()
+        {
+            Add("include", Callbacks.Include);
+            Add("define", Callbacks.Define);
+        }
+        public static void Add(string name, DirectiveCallback callback)
+        {
+            DirectiveList.Add(new Directive(name, callback));
+        }
+    }
+
+    public class Directive
+    {
+        public string Name;
+        public DirectiveCallback callback;
+
+        public Directive(string name, DirectiveCallback Callback) 
+        {
+            Name = name;
+            callback = Callback;
+        }
+    }
+
+    public static class Callbacks
+    {
+        internal static void Include(string line)
+        {
+            string temp = line;
+            string file = "";
+            temp = temp.Replace("#include", "");
+            if (temp.StartsWith("\""))
+            {
+                temp.Replace("\"", "");
+                if (File.Exists(Path.Combine(Path.GetDirectoryName(Preprocessor.FilePath), temp)))
+                    file = File.ReadAllText(Path.Combine(Path.GetDirectoryName(Preprocessor.FilePath), temp));
+                else Exceptions.Exception(7);
+            }
+            else if (temp.StartsWith("<"))
+            {
+                temp.Replace("<", "");
+                temp.Replace(">", "");
+                if (File.Exists(Path.Combine(Path.Combine(Environment.CurrentDirectory, "\\include"), temp)))
+                    file = File.ReadAllText(Path.Combine(Path.Combine(Environment.CurrentDirectory, "\\include"), temp));
+                else Exceptions.Exception(7);
+            }
+            else
+            {
+                Exceptions.Exception(8);
+                return;
+            }
+
+            if (!file.StartsWith("#Halcyon"))
+            {
+                Exceptions.Exception(9);
+            }
+            else
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append(Preprocessor.PreprocessedFile);
+                sb.Append(file.Replace("#Halcyon", ""));
+            }
+        }
+
+        internal static void Define(string line)
+        {
+            string temp = line.Replace("#define", "");
+            string key = temp.Split(Convert.ToChar(" ")).First();
+            string value = String.Join(" ", temp.Replace(key, "").Split(Convert.ToChar(" ")));
+            Preprocessor.PreprocessedFile = Preprocessor.PreprocessedFile.Replace(key, value); 
+        }
+    }
+}
