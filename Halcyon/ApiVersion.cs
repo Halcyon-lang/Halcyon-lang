1ca0298976c55387491d617baaa0c9eaa0d775ac
magnusi <luk.hozda@gmail.com>
2015-08-21 00:05:49 +0200

    Added ApiVersion

---
+++ Halcyon/ApiVersion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SecretsOfLaurens
+{
+    class ApiVersion
+    {
+        public static int Major = 0;
+        public static int Minor = 1;
+    }
+}
