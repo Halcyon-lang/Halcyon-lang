4f17e4c58dd37e6b139a2fe325515fb6c7a50456
magnusi <luk.hozda@gmail.com>
2015-08-21 00:20:56 +0200

    Attribute added, ExtensionLoader comming in a bit

---
+++ Halcyon/ApiVersionAttribute.cs
@@ -0,0 +1,36 @@
+﻿/*
+This class is part of modification of TerrariaServerApi
+Copyright (C) 2011-2015 Nyx Studios (fka. The TShock Team)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Halcyon
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class ApiVersionAttribute : Attribute
+    {
+        public Version ApiVersion;
+        public ApiVersionAttribute(Version version)
+        {
+            this.ApiVersion = version;
+        }
+        public ApiVersionAttribute(int major, int minor)
+            : this(new Version(major, minor))
+        {
+        }
+    }
+}
