6ebc0cfed5230a540d31fbff50f34d8a47c44602
luciusmagn <luk.hozda@gmail.com>
2015-08-23 11:46:06 +0200

    Removed every C thing. Not using C for speed improvement until actually needed or until it doesn't cause System.AccessViolationException

--- HalcyonC/dllheader.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define Dll   __declspec( dllexport )
-
-int main() 
-{
-    return 0;
-}
--- buildC.bat
+++
@@ -1,4 +0,0 @@
-tcc\tcc -o tcc-output\Utils.dll HalcyonC\Utils.c
-@echo off
-echo "Build completed!"
-pause
