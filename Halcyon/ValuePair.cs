e83df140902b69e76ddff5f832dc4b67e0a98576
luciusmagn <luk.hozda@gmail.com>
2015-08-25 12:52:08 +0200

    Added ValuePair. It is a "collection"-ish thing which stores one pair of values of any type. Like Dictionary but only one KeyValuePair

---
+++ Halcyon/ValuePair.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Halcyon
+{
+    public class ValuePair<Left, Right>
+    {
+        Left LeftValue;
+        Right RightValue;
+        public ValuePair(Left l, Right r)
+        {
+            SetLeft(l);
+            SetRight(r);
+        }
+
+        public void SetRight(Right r)
+        {
+            if (r != null)
+            {
+                RightValue = r;
+            }
+            else
+            {
+                throw new System.NullReferenceException();
+            }
+        }
+
+        public void SetLeft(Left l)
+        {
+            if (l != null)
+            {
+                LeftValue = l;
+            }
+            else
+            {
+                throw new System.NullReferenceException();
+            }
+        }
+    }
+}
