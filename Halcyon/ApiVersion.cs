151e79b1bc16a8cae7295ddbd0f6aac56697b9a1
luciusmagn <luk.hozda@gmail.com>
2015-08-28 19:57:33 +0200

    Updated versions and added moar. Also summaries

--- Halcyon/ApiVersion.cs
+++ Halcyon/ApiVersion.cs
@@ -5,11 +5,34 @@ using System.Text;
 
 namespace Halcyon
 {
-    class ApiVersion
+    /// <summary>
+    /// Contains versions of various Halcyon components and Halcyon itself
+    /// </summary>
+    public class ApiVersion
     {
+        /// <summary>
+        /// Halcyon's major version
+        /// </summary>
         public static int Major = 0;
-        public static int Minor = 1;
-        public static int ILasmMinor = 0;
-        public static int ILasmMajor = 1;
+        /// <summary>
+        /// Halcyon's minor version
+        /// </summary>
+        public static int Minor = 3;
+        /// <summary>
+        /// IL Assembler (ILasm) wrapper major version
+        /// </summary>
+        public static int ILasmMajor = 0;
+        /// <summary>
+        /// IL Assembler (ILasm) wrapper minor version
+        /// </summary>
+        public static int ILasmMinor = 2;
+        /// <summary>
+        /// AssemblyLinker (AL) wrapper major version
+        /// </summary>
+        public static int ALMajor = 0;
+        /// <summary>
+        /// AssemblyLinker (AL) wrapper minor version
+        /// </summary>
+        public static int ALMinor = 1;
     }
 }
